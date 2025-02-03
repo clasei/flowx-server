bbf4359e8039c6aa2c557413b2f7befec8e8fd4f
clara seijo <clasei@proton.me>
2025-02-03 20:52:51 +0100

    initialized Spring Boot project + updated .gitignore

---
+++ src/main/java/com/flowx/flowx_server/FlowxServerApplication.java
@@ -0,0 +1,13 @@
+package com.flowx.flowx_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowxServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowxServerApplication.class, args);
+	}
+
+}
